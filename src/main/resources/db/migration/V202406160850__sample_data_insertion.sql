70f44f1f92c37bdbc081d105e6fb4c28a0b7a8ae
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-04 21:52:24 +0300

    Add new sample data for instruments and corresponding portfolio transactions

--- src/main/resources/db/migration/V202406160850__sample_data_insertion.sql
+++ src/main/resources/db/migration/V202406160850__sample_data_insertion.sql
@@ -1,6 +1,16 @@
 -- Sample data insertion
 INSERT INTO instrument (symbol, name, instrument_category, base_currency)
 VALUES ('QDVE.DEX', 'iShares S&P 500 Information Technology Sector UCITS ETF USD (Acc)', 'ETF', 'EUR');
+INSERT INTO instrument (symbol, name, instrument_category, base_currency)
+VALUES ('EXXT', 'iShares NASDAQ-100® UCITS ETF (DE)', 'ETF', 'USD');
+INSERT INTO instrument (symbol, name, instrument_category, base_currency)
+VALUES ('NQSE', 'iShares NASDAQ 100 UCITS ETF', 'ETF', 'EUR');
 
 INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date)
 VALUES ((SELECT id FROM instrument WHERE symbol = 'QDVE.DEX'), 'BUY', 3.37609300, 29.62003713, '2024-07-01');
+
+INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date)
+VALUES ((SELECT id FROM instrument WHERE symbol = 'EXXT'), 'BUY', 1, 196.18, '2024-07-04');
+
+INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date)
+VALUES ((SELECT id FROM instrument WHERE symbol = 'NQSE'), 'BUY', 8.2304526749, 12.15, '2024-07-04');
