ae3a98c1abc1ba2a5f9df5155004ce4782858719
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-05 09:02:31 +0300

    Add ID to "Add New Instrument" button and update test accordingly

--- src/test/kotlin/e2e/InstrumentSavingE2ETest.kt
+++ src/test/kotlin/e2e/InstrumentSavingE2ETest.kt
@@ -1,8 +1,12 @@
 package e2e
 
+import com.codeborne.selenide.Condition
 import com.codeborne.selenide.Condition.text
+import com.codeborne.selenide.Condition.visible
+import com.codeborne.selenide.Selectors.withText
 import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.Selenide.element
+import com.codeborne.selenide.Selenide.elements
 import com.codeborne.selenide.Selenide.open
 import com.codeborne.selenide.SelenideElement
 import org.assertj.core.api.Assertions.assertThat
@@ -10,13 +14,14 @@ import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
 import org.openqa.selenium.By.className
+import org.openqa.selenium.By.tagName
 
 class InstrumentSavingE2ETest {
 
   companion object {
     private const val DEFAULT_SYMBOL = "AAPL"
     private const val DEFAULT_NAME = "Apple Inc."
-    private const val DEFAULT_CATEGORY = "STOCKS"
+    private const val DEFAULT_CATEGORY = "Stock"
     private const val DEFAULT_CURRENCY = "USD"
   }
 
@@ -24,10 +29,13 @@ class InstrumentSavingE2ETest {
   fun `should display success message when saving instrument with valid data`() {
     open("http://localhost:61234")
 
+    id("addNewInstrument").click()
     id("symbol").shouldNotHave(text(DEFAULT_SYMBOL)).setValue(DEFAULT_SYMBOL)
     id("name").shouldNotHave(text(DEFAULT_NAME)).setValue(DEFAULT_NAME)
-    id("category").shouldNotHave(text(DEFAULT_CATEGORY)).setValue(DEFAULT_CATEGORY)
-    id("currency").shouldNotHave(text(DEFAULT_CURRENCY)).setValue(DEFAULT_CURRENCY).pressEnter()
+    id("category").selectOption(DEFAULT_CATEGORY)
+    id("currency").selectOption(DEFAULT_CURRENCY)
+
+    elements(tagName("button")).filter(text("Save")).first().click()
 
     Selenide.sleep(1000)
 
