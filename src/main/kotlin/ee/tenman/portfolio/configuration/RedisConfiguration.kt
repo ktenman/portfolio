dd081609e791a99755849e989ce37d800662ddb6
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-06 17:11:45 +0300

    Reduce TTL for TRANSACTION_CACHE from 1 day to 1 hour

--- src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
+++ src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
@@ -18,7 +18,7 @@ class RedisConfiguration {
     cacheConfigurations[INSTRUMENT_CACHE] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(366))
     cacheConfigurations[THIRTY_MINUTES] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(30))
     cacheConfigurations[SUMMARY_CACHE] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1))
-    cacheConfigurations[TRANSACTION_CACHE] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1))
+    cacheConfigurations[TRANSACTION_CACHE] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1))
     val defaultConfig = RedisCacheConfiguration.defaultCacheConfig().entryTtl(DEFAULT_TTL)
     return RedisCacheManager.builder(connectionFactory!!)
       .cacheDefaults(defaultConfig)
