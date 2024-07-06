627a19bc5e8d6a25733035fd3b842a64c69c80cb
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-06 14:06:58 +0300

    Add id attribute to the "Add New Instrument" button in instrument-component.vue

--- ui/components/instrument-component.vue
+++ ui/components/instrument-component.vue
@@ -2,7 +2,7 @@
   <div class="container mt-3">
     <div class="d-flex justify-content-between">
       <h4>Portfolio Transactions</h4>
-      <button class="btn btn-primary mb-3" @click="showAddInstrumentModal">
+      <button class="btn btn-primary mb-3" id="addNewInstrument" @click="showAddInstrumentModal">
         Add New Instrument
       </button>
     </div>
