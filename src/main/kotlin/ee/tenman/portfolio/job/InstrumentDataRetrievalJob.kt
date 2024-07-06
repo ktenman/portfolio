3fa2731850bd6edebd2be15e5a6757340f0fea3c
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-06 09:30:04 +0300

    Update docker-compose, cron schedule, and Java memory settings in backend

--- src/main/kotlin/ee/tenman/portfolio/job/InstrumentDataRetrievalJob.kt
+++ src/main/kotlin/ee/tenman/portfolio/job/InstrumentDataRetrievalJob.kt
@@ -18,7 +18,7 @@ class InstrumentDataRetrievalJob(
 ) {
   private val log = LoggerFactory.getLogger(javaClass)
 
-  @Scheduled(cron = "0 0 3,21 * * *")
+  @Scheduled(cron = "0 0 3,6,18,21 * * *")
   fun retrieveInstrumentData() {
     log.info("Starting instrument data retrieval job")
     val instruments = instrumentService.getAllInstruments()
