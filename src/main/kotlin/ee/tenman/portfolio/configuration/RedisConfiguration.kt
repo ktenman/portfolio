a7ea605502ae2cb3c68bc643681e3f503ef94cde
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-08 00:49:42 +0300

    Update cache constants to version v3 for improved cache management

--- src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
+++ src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
@@ -27,11 +27,11 @@ class RedisConfiguration {
   }
 
   companion object {
-    const val INSTRUMENT_CACHE = "instrument-cache"
-    const val SUMMARY_CACHE = "summary-cache-v2"
-    const val TRANSACTION_CACHE = "transaction-cache"
-    const val ONE_DAY_CACHE: String = "one-day-cache"
-    const val THIRTY_MINUTES: String = "thirty-minutes-cache"
+    const val INSTRUMENT_CACHE = "instrument-cache-v3"
+    const val SUMMARY_CACHE = "summary-cache-v3"
+    const val TRANSACTION_CACHE = "transaction-cache-v3"
+    const val ONE_DAY_CACHE: String = "one-day-cache-v3"
+    const val THIRTY_MINUTES: String = "thirty-minutes-cache-v3"
     private val DEFAULT_TTL: Duration = Duration.ofMinutes(30)
   }
 }
