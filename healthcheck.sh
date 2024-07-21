223b67846dfcf21d288ef3c7d741c3682db5f579
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-21 17:16:35 +0300

    Remove unused files

--- healthcheck.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pg_isready -U "$(cat /run/secrets/postgres_user)" -d portfolio
--- set_pg_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export POSTGRES_USER=$(cat /run/secrets/postgres_user)
-export POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password)
