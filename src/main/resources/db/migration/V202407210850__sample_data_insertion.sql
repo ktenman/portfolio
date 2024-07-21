ab4fd16e4ba7ab6db90fb575d85a69528ce0cc66
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-21 16:00:39 +0300

    Add healthcheck and update config in docker-stack, insert sample data, update cache v10

--- src/main/resources/db/migration/V202407210850__sample_data_insertion.sql
+++ src/main/resources/db/migration/V202407210850__sample_data_insertion.sql
@@ -1,3 +1,3 @@
 -- Sample data insertion
 INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date)
-VALUES ((SELECT id FROM instrument WHERE symbol = 'QDVE.DEX'), 'BUY', 6, 866949, 29.125, '2024-07-19');
+VALUES ((SELECT id FROM instrument WHERE symbol = 'QDVE.DEX'), 'BUY', 6.866949, 29.125, '2024-07-19');
