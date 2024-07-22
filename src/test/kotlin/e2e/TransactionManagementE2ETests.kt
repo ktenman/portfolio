0437be17b63da07dab57b79ed4471e047af16537
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-22 23:28:21 +0300

    Add setUp method to initialize browser state before each E2E test

--- src/test/kotlin/e2e/TransactionManagementE2ETests.kt
+++ src/test/kotlin/e2e/TransactionManagementE2ETests.kt
@@ -6,7 +6,7 @@ import com.codeborne.selenide.SelenideElement
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
 
@@ -14,18 +14,20 @@ private const val TRANSACTIONS_BASE_URL = "http://localhost:61234/transactions"
 
 class TransactionManagementE2ETests {
 
+  @BeforeEach
+  fun setUp() {
+    Selenide.open(TRANSACTIONS_BASE_URL)
+  }
+
   @AfterEach
   fun tearDown() {
     Selenide.clearBrowserLocalStorage()
   }
 
-  @Disabled
   @Test
   fun `should display success message after saving a new transaction`() {
-    Selenide.open(TRANSACTIONS_BASE_URL)
-
     id("addNewTransaction").click()
-    id("instrumentId").selectOption("QDVE.DEX - iShares S&P 500 Information Technology Sector")
+    id("instrumentId").selectOption("AAPL - Apple Inc.")
     id("transactionType").selectOption("Buy")
     id("quantity").setValue("10.144")
     id("price").setValue("29.615")
@@ -42,21 +44,18 @@ class TransactionManagementE2ETests {
       fail("Alert message not found.")
     }
 
-    val qdveTransaction = Selenide.elements(By.tagName("td")).findBy(Condition.text("QDVE.DEX"))
+    val qdveTransaction = Selenide.elements(By.tagName("td")).findBy(Condition.text("AAPL"))
     if (qdveTransaction.exists()) {
-      assertThat(qdveTransaction.text).isEqualTo("QDVE.DEX")
+      assertThat(qdveTransaction.text).isEqualTo("AAPL")
     } else {
       fail("Transaction not found.")
     }
   }
 
-  @Disabled
   @Test
   fun `should display success message after editing an existing transaction`() {
-    Selenide.open(TRANSACTIONS_BASE_URL)
-
     Selenide.elements(By.tagName("button")).filter(Condition.text("Edit")).first().click()
-    id("instrumentId").selectOption("QDVE.DEX - iShares S&P 500 Information Technology Sector")
+    id("instrumentId").selectOption("AAPL - Apple Inc.")
     id("transactionType").selectOption("Sell")
     id("quantity").setValue("112255.1211")
     id("price").setValue("332211.189")
@@ -77,7 +76,7 @@ class TransactionManagementE2ETests {
       .closest("tr").findAll(By.tagName("td"))
 
     assertThat(transactionDetails.texts()).containsExactlyInAnyOrder(
-      "QDVE.DEX",
+      "AAPL",
       "SELL",
       "112255.12",
       "332211.19",
@@ -87,4 +86,6 @@ class TransactionManagementE2ETests {
   }
 
   private fun id(id: String): SelenideElement = Selenide.element(By.id(id))
+
+
 }
