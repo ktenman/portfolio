e0a2c6e1ca81fd67f77e508b81a7eeea607396a4
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-23 08:56:37 +0300

    Format import statements for better readability in main.ts

--- ui/main.ts
+++ ui/main.ts
@@ -1,14 +1,14 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './app.vue'
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import router from './router'
 
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import {faPencilAlt} from '@fortawesome/free-solid-svg-icons'
-import {faTrashAlt} from '@fortawesome/free-solid-svg-icons/faTrashAlt'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons/faTrashAlt'
 
 library.add(faPencilAlt)
 library.add(faTrashAlt)
