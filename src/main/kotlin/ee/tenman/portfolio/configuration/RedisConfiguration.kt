be5fa849c44b1506fa3d107b8438e8c23bbc5e6b
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-25 23:32:12 +0300

    Add caching for current day summary and new 15-minute summary cache

--- src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
+++ src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
@@ -18,6 +18,7 @@ class RedisConfiguration {
     cacheConfigurations[INSTRUMENT_CACHE] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(366))
     cacheConfigurations[THIRTY_MINUTES] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(30))
     cacheConfigurations[SUMMARY_CACHE] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1))
+    cacheConfigurations[SUMMARY_CACHE_15] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(15))
     cacheConfigurations[TRANSACTION_CACHE] = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1))
     val defaultConfig = RedisCacheConfiguration.defaultCacheConfig().entryTtl(DEFAULT_TTL)
     return RedisCacheManager.builder(connectionFactory)
@@ -29,6 +30,7 @@ class RedisConfiguration {
   companion object {
     const val INSTRUMENT_CACHE = "instrument-cache-v13"
     const val SUMMARY_CACHE = "summary-cache-v13"
+    const val SUMMARY_CACHE_15 = "summary-cache-15-v13"
     const val TRANSACTION_CACHE = "transaction-cache-v13"
     const val ONE_DAY_CACHE: String = "one-day-cache-v13"
     const val THIRTY_MINUTES: String = "thirty-minutes-cache-v13"
