f03ba2a880e859957099a6e4196af14f407888ac
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-27 07:56:53 +0300

    Update Clock configuration to Europe/Tallinn and reschedule XIRR job to 6 AM

--- src/main/kotlin/ee/tenman/portfolio/configuration/ClockConfiguration.kt
+++ src/main/kotlin/ee/tenman/portfolio/configuration/ClockConfiguration.kt
@@ -3,10 +3,10 @@ package ee.tenman.portfolio.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Clock
+import java.time.ZoneId
 
 @Configuration
 class ClockConfiguration {
-
   @Bean
-  fun clock(): Clock = Clock.systemUTC()
+  fun clock(): Clock = Clock.system(ZoneId.of("Europe/Tallinn"))
 }
