e654b57c8fd94a96c12117c9b7883588bad5d35c
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-28 21:51:28 +0300

    Add investment calculator component and route to the portfolio app

--- ui/router/index.ts
+++ ui/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import InstrumentComponent from '../components/instrument-component.vue'
 import PortfolioTransactionComponent from '../components/transaction-component.vue'
 import PortfolioSummaryComponent from '../components/portfolio-summary-component.vue'
+import CalculatorComponent from '../components/calculator-component.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,6 +19,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/instruments',
     name: 'Instruments',
     component: InstrumentComponent,
+  },
+  {
+    path: '/calculator',
+    name: 'Calculator',
+    component: CalculatorComponent,
   },
   {
     path: '/:pathMatch(.*)*',
