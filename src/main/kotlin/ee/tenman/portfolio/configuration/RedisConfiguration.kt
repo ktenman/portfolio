08a02ef2513a3dea2b26a2f60473fcc3968e7e72
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-31 10:39:15 +0300

    Update redis cache configuration

--- src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
+++ src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
@@ -28,12 +28,12 @@ class RedisConfiguration {
   }
 
   companion object {
-    const val INSTRUMENT_CACHE = "instrument-cache-v18"
-    const val SUMMARY_CACHE = "summary-cache-v18"
-    const val SUMMARY_CACHE_15 = "summary-cache-15-v18"
-    const val TRANSACTION_CACHE = "transaction-cache-v18"
-    const val ONE_DAY_CACHE: String = "one-day-cache-v18"
-    const val THIRTY_MINUTES: String = "thirty-minutes-cache-v18"
+    const val INSTRUMENT_CACHE = "instrument-cache-v19"
+    const val SUMMARY_CACHE = "summary-cache-v19"
+    const val SUMMARY_CACHE_15 = "summary-cache-15-v19"
+    const val TRANSACTION_CACHE = "transaction-cache-v19"
+    const val ONE_DAY_CACHE: String = "one-day-cache-v19"
+    const val THIRTY_MINUTES: String = "thirty-minutes-cache-v19"
     private val DEFAULT_TTL: Duration = Duration.ofMinutes(30)
   }
 }
