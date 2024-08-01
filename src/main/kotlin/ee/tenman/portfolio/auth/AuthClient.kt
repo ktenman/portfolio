ec99b378ae0e43f4715e80eb6571a5987006e4f9
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-08-01 19:38:23 +0300

    Remove unnecessary headers from AuthClient's getUser method

--- src/main/kotlin/ee/tenman/portfolio/auth/AuthClient.kt
+++ src/main/kotlin/ee/tenman/portfolio/auth/AuthClient.kt
@@ -5,19 +5,15 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(name = AuthClient.CLIENT_NAME, url = "\${auth.url}")
 interface AuthClient {
   @GetMapping(
     "/user-by-session",
-    consumes = [MediaType.APPLICATION_JSON_VALUE],
     produces = [MediaType.APPLICATION_JSON_VALUE]
   )
   fun getUser(
-    @RequestParam sessionId: String,
-    @RequestHeader("Accept") accept: String = MediaType.APPLICATION_JSON_VALUE,
-    @RequestHeader("Content-Type") contentType: String = MediaType.APPLICATION_JSON_VALUE
+    @RequestParam sessionId: String
   ): AuthResponse
 
   companion object {
