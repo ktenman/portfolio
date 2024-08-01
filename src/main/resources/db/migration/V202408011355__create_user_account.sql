3c4ea94127de19555aec334699878f6895c6d614
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-08-01 14:08:41 +0300

    Make session_id unique and add an index on it in user_account table

--- src/main/resources/db/migration/V202408011355__create_user_account.sql
+++ src/main/resources/db/migration/V202408011355__create_user_account.sql
@@ -2,7 +2,9 @@ CREATE TABLE user_account
 (
   id         BIGSERIAL PRIMARY KEY,
   email      VARCHAR(255)                                       NOT NULL UNIQUE,
-  session_id VARCHAR(255),
+  session_id VARCHAR(255) UNIQUE,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+CREATE INDEX idx_user_account_session_id ON user_account (session_id);
