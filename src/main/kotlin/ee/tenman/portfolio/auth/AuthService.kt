e59f1ff747d8fed09a416411ed87878c2888b9cb
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-08-01 20:09:31 +0300

    Enable caching for session ID retrieval in UserAccountService and AuthService

--- src/main/kotlin/ee/tenman/portfolio/auth/AuthService.kt
+++ src/main/kotlin/ee/tenman/portfolio/auth/AuthService.kt
@@ -4,15 +4,13 @@ import ee.tenman.portfolio.auth.model.AuthResponse
 import ee.tenman.portfolio.configuration.RedisConfiguration.Companion.USER_SESSION_CACHE
 import org.slf4j.LoggerFactory
 import org.springframework.cache.annotation.Cacheable
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.server.ResponseStatusException
 
 @Service
 class AuthService(private val authClient: AuthClient) {
   private val log = LoggerFactory.getLogger(javaClass)
 
-  //  @Cacheable(value = [USER_SESSION_CACHE], key = "#sessionId")
+  @Cacheable(value = [USER_SESSION_CACHE], key = "#sessionId")
   fun getAuthResponse(sessionId: String): AuthResponse {
     log.info("Checking session: $sessionId")
     val response = authClient.getUser(sessionId)
