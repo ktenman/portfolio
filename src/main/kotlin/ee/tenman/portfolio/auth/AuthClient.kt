f360d04466bb69cbe4ffcaa7ac24e242293601e4
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-08-01 19:14:49 +0300

    Refactor AuthClient to return AuthResponse directly, removing status checks

--- src/main/kotlin/ee/tenman/portfolio/auth/AuthClient.kt
+++ src/main/kotlin/ee/tenman/portfolio/auth/AuthClient.kt
@@ -3,7 +3,6 @@ package ee.tenman.portfolio.auth
 import ee.tenman.portfolio.auth.model.AuthResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -14,7 +13,7 @@ interface AuthClient {
     consumes = [MediaType.APPLICATION_JSON_VALUE],
     produces = [MediaType.APPLICATION_JSON_VALUE]
   )
-  fun getUser(@RequestParam sessionId: String): ResponseEntity<AuthResponse>
+  fun getUser(@RequestParam sessionId: String): AuthResponse
 
   companion object {
     const val CLIENT_NAME = "authClient"
