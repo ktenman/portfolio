80757e6d9b6f7b817de42a7b6628a8261183c1ee
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-08-01 18:11:25 +0300

    Add Jackson annotations to auth models and log session ID in DefaultAuthFilter

--- src/main/kotlin/ee/tenman/portfolio/auth/model/AuthStatus.kt
+++ src/main/kotlin/ee/tenman/portfolio/auth/model/AuthStatus.kt
@@ -1,5 +1,11 @@
 package ee.tenman.portfolio.auth.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 enum class AuthStatus {
-    AUTHORIZED, UNAUTHORIZED
+  @JsonProperty("AUTHORIZED")
+  AUTHORIZED,
+
+  @JsonProperty("UNAUTHORIZED")
+  UNAUTHORIZED
 }
