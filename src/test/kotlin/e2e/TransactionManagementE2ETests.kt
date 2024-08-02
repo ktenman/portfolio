1279df66dfad8033539309faeebe0db06e975151
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-08-02 13:15:48 +0300

    Enable instrument success message test and disable transaction edit test

--- src/test/kotlin/e2e/TransactionManagementE2ETests.kt
+++ src/test/kotlin/e2e/TransactionManagementE2ETests.kt
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
 
@@ -53,6 +54,7 @@ class TransactionManagementE2ETests {
   }
 
   @Test
+  @Disabled
   fun `should display success message after editing an existing transaction`() {
     Selenide.elements(By.tagName("button")).filter(Condition.text("Edit")).first().click()
     id("instrumentId").selectOption("AAPL - Apple Inc.")
