603d0dcc5eceb3a22b675e78eb04e273a737e960
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-08-07 23:13:19 +0300

    Add import for CalculationResult in calculation-service.ts

--- ui/services/calculation-service.ts
+++ ui/services/calculation-service.ts
@@ -1,6 +1,7 @@
 import { ApiClient } from './api-client.ts'
 import { CACHE_KEYS } from '../constants/cache-keys.ts'
 import { Cacheable } from '../decorators/cacheable.decorator.ts'
+import { CalculationResult } from '../models/calculation-result.ts'
 
 export class CalculationService {
   private apiUrl = '/api/calculator/calculate'
