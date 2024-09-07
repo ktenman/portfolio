765cc7e2b4652faa2414f28d4b6201d6553f4cf4
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-09-08 00:29:26 +0300

    Exclude node_modules directory from README link checks

--- .github/workflows/check-readme-links.sh
+++ .github/workflows/check-readme-links.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-for f in `find . -name '*.md'`; do
+for f in $(find . -name '*.md' -not -path "./node_modules/*"); do
   echo "Checking $f"
-  PATHS=`grep -oP "(?<=\]\()[^)]*(?=\))" $f | grep -v '^http'`
+  PATHS=$(grep -oP "(?<=\]\()[^)]*(?=\))" "$f" | grep -v '^http')
   for l in $PATHS; do
-    path=`dirname $f`/$l
+    path=$(dirname "$f")/$l
     if [ ! -e "$path" ]; then
       echo "$path doesn't exist" && exit 1
     fi
