285b1685148f8f6922ddfa0cbef15a465b0fc615
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-10-06 17:15:11 +0300

    Fix inconsistent indentation in portfolio-summary.ts and calculator-component.vue

--- ui/components/calculator-component.vue
+++ ui/components/calculator-component.vue
@@ -35,17 +35,17 @@
         <h5>Year-by-Year Summary</h5>
         <table class="table table-striped table-hover">
           <thead>
-          <tr>
-            <th v-for="header in tableHeaders" :key="header">{{ header }}</th>
-          </tr>
+            <tr>
+              <th v-for="header in tableHeaders" :key="header">{{ header }}</th>
+            </tr>
           </thead>
           <tbody>
-          <tr v-for="summary in yearSummary" :key="summary.year">
-            <td>{{ summary.year }}</td>
-            <td v-for="key in ['totalWorth', 'yearGrowth', 'earningsPerMonth']" :key="key">
-              {{ formatCurrency(summary[key]) }}
-            </td>
-          </tr>
+            <tr v-for="summary in yearSummary" :key="summary.year">
+              <td>{{ summary.year }}</td>
+              <td v-for="key in ['totalWorth', 'yearGrowth', 'earningsPerMonth']" :key="key">
+                {{ formatCurrency(summary[key]) }}
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
