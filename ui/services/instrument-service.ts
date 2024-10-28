48192acae9f4d244444d452681fb2107684d654d
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-10-28 11:54:57 +0200

    Add caching

--- ui/services/instrument-service.ts
+++ ui/services/instrument-service.ts
@@ -1,21 +1,29 @@
 import { Instrument } from '../models/instrument'
+import { Cacheable } from '../decorators/cacheable.decorator'
+import { CachePut } from '../decorators/cache-put.decorator'
+import { CacheEvict } from '../decorators/cache-evict.decorator'
+import { CACHE_KEYS } from '../constants/cache-keys'
 import { ApiClient } from './api-client.ts'
 
 export class InstrumentService {
   private apiUrl = '/api/instruments'
 
+  @Cacheable(CACHE_KEYS.INSTRUMENTS)
   async getAllInstruments(): Promise<Instrument[]> {
     return ApiClient.get<Instrument[]>(this.apiUrl)
   }
 
+  @CachePut(CACHE_KEYS.INSTRUMENTS)
   async saveInstrument(instrument: Instrument): Promise<Instrument> {
     return ApiClient.post<Instrument>(this.apiUrl, instrument)
   }
 
+  @CacheEvict(CACHE_KEYS.INSTRUMENTS)
   async updateInstrument(id: number, instrument: Instrument): Promise<Instrument> {
     return ApiClient.put<Instrument>(`${this.apiUrl}/${id}`, instrument)
   }
 
+  @CacheEvict(CACHE_KEYS.INSTRUMENTS)
   async deleteInstrument(id: number): Promise<void> {
     await ApiClient.delete(`${this.apiUrl}/${id}`)
   }
