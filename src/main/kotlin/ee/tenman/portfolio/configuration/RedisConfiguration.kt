6895063284dce4aaf39aee4cbe2bfc8f085b9c2e
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-10-28 08:25:23 +0200

    Update cache keys to version 4 for consistency across services

--- src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
+++ src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
@@ -26,10 +26,10 @@ class RedisConfiguration {
   }
 
   companion object {
-    const val INSTRUMENT_CACHE = "instrument-cache-v3"
-    const val SUMMARY_CACHE = "summary-cache-v3"
-    const val TRANSACTION_CACHE = "transaction-cache-v3"
-    const val ONE_DAY_CACHE: String = "one-day-cache-v3"
+    const val INSTRUMENT_CACHE = "instrument-cache-v4"
+    const val SUMMARY_CACHE = "summary-cache-v4"
+    const val TRANSACTION_CACHE = "transaction-cache-v4"
+    const val ONE_DAY_CACHE: String = "one-day-cache-v4"
     private val DEFAULT_TTL: Duration = Duration.ofMinutes(10)
   }
 }
