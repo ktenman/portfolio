aef2449365faf22c9db58268d1defd1c7e5cde55
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-12-08 19:25:41 +0200

    Disable telegram bot in tests

--- src/main/kotlin/ee/tenman/portfolio/telegram/TelegramBotConfig.kt
+++ src/main/kotlin/ee/tenman/portfolio/telegram/TelegramBotConfig.kt
@@ -1,5 +1,6 @@
 package ee.tenman.portfolio.telegram
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.telegram.telegrambots.meta.TelegramBotsApi
@@ -7,12 +8,16 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
 @Configuration
 class TelegramBotConfig {
+
   @Bean
+  @ConditionalOnProperty(name = ["telegram.bot.enabled"], havingValue = "true", matchIfMissing = true)
   fun telegramBotsApi(): TelegramBotsApi = TelegramBotsApi(DefaultBotSession::class.java)
 
   @Bean
+  @ConditionalOnProperty(name = ["telegram.bot.enabled"], havingValue = "true", matchIfMissing = true)
   fun registerBot(bot: CarTelegramBot, api: TelegramBotsApi): TelegramBotsApi {
     api.registerBot(bot)
     return api
   }
 }
+
