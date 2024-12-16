745d6db9dd1cef7b93765b97b6551a09023c2ff1
Konstantin Tenman <konstantin.tenman@cyber.ee>
2024-12-16 16:33:22 +0200

    Add missing file

---
+++ ui/models/platform.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+export enum Platform {
+  AVIVA = 'Aviva',
+  BINANCE = 'Binance',
+  COINBASE = 'Coinbase',
+  LHV = 'LHV',
+  LIGHTYEAR = 'Lightyear',
+  SWEDBANK = 'Swedbank',
+  TRADING212 = 'Trading212',
+  UNKNOWN = 'Unknown',
+}
+/* eslint-enable no-unused-vars */
