01bfb11712d68fc9d14af6ad3a23741c636894c6
Konstantin Tenman <ktenman@users.noreply.github.com>
2025-01-30 20:14:17 +0200

    Fix calculation logic

--- src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
+++ src/main/kotlin/ee/tenman/portfolio/configuration/RedisConfiguration.kt
@@ -26,10 +26,10 @@ class RedisConfiguration {
   }
 
   companion object {
-    const val INSTRUMENT_CACHE = "instrument-cache-v24"
-    const val SUMMARY_CACHE = "summary-cache-v24"
-    const val TRANSACTION_CACHE = "transaction-cache-v24"
-    const val ONE_DAY_CACHE: String = "one-day-cache-v24"
+    const val INSTRUMENT_CACHE = "instrument-cache-v25"
+    const val SUMMARY_CACHE = "summary-cache-v25"
+    const val TRANSACTION_CACHE = "transaction-cache-v25"
+    const val ONE_DAY_CACHE: String = "one-day-cache-v25"
     private val DEFAULT_TTL: Duration = Duration.ofMinutes(10)
   }
 }
