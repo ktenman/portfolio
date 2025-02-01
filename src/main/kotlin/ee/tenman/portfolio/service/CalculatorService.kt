221aa3714425f3202b6d1ef8aaf56687df5558fd
Konstantin Tenman <ktenman@users.noreply.github.com>
2025-02-01 15:19:46 +0200

    Update cache

--- src/main/kotlin/ee/tenman/portfolio/service/CalculatorService.kt
+++ src/main/kotlin/ee/tenman/portfolio/service/CalculatorService.kt
@@ -38,7 +38,7 @@ class CalculatorService(
     }
   }
 
-  @Cacheable(value = [ONE_DAY_CACHE], key = "'xirr-v25'")
+  @Cacheable(value = [ONE_DAY_CACHE], key = "'xirr+v1'")
   fun getCalculationResult(): CalculationResult {
     log.info("Calculating XIRR")
     val xirrs = calculateRollingXirr(TICKER).reversed()
