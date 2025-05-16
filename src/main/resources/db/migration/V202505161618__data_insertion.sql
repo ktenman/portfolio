3f516d94946c5f08fa94b8aff0b55cf9941a1c13
Konstantin Tenman <ktenman@users.noreply.github.com>
2025-05-16 16:20:05 +0300

    Update Redis cache versioning and add data insertion SQL script

---
+++ src/main/resources/db/migration/V202505161618__data_insertion.sql
@@ -0,0 +1,2 @@
+INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date, platform)
+VALUES ((SELECT id FROM instrument WHERE symbol = 'QDVE:GER:EUR'), 'BUY',  40, 30.008, '2025-05-16', 'SWEDBANK');
