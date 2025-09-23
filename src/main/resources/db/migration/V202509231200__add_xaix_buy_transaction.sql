59126a895c6e63a2b56f95353a04740eac729df0
Konstantin Tenman <konstantin.tenman@cyber.ee>
2025-09-23 15:54:45 +0300

    Update Redis cache versioning and add XAIX buy transaction to portfolio

---
+++ src/main/resources/db/migration/V202509231200__add_xaix_buy_transaction.sql
@@ -0,0 +1,2 @@
+INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date, platform)
+VALUES ((SELECT id FROM instrument WHERE symbol = 'XAIX:GER:EUR'), 'BUY', 42, 150.50, '2025-09-23', 'SWEDBANK');
