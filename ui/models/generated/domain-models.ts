a1658636fd35f650fcac2050e9e67bbd1bd7f979
Konstantin Tenman <konstantin.tenman@cyber.ee>
2025-11-13 16:48:05 +0200

    Add data transfer objects for financial instruments and transactions

---
+++ ui/models/generated/domain-models.ts
@@ -0,0 +1,117 @@
+/* tslint:disable */
+/* eslint-disable */
+// Generated using typescript-generator (timestamp removed to prevent git churn)
+
+/**
+ * Financial instrument data transfer object
+ */
+export interface InstrumentDto {
+    id: number | null;
+    symbol: string;
+    name: string;
+    category: string;
+    baseCurrency: string;
+    currentPrice: number | null;
+    quantity: number | null;
+    providerName: string;
+    totalInvestment: number | null;
+    currentValue: number | null;
+    profit: number | null;
+    realizedProfit: number | null;
+    unrealizedProfit: number | null;
+    xirr: number | null;
+    platforms: string[];
+    priceChangeAmount: number | null;
+    priceChangePercent: number | null;
+}
+
+export interface TransactionRequestDto {
+    id: number | null;
+    instrumentId: number;
+    transactionType: TransactionType;
+    quantity: number;
+    price: number;
+    transactionDate: DateAsString;
+    platform: Platform;
+    commission: number;
+    currency: string;
+}
+
+export interface TransactionResponseDto {
+    id: number | null;
+    instrumentId: number;
+    symbol: string;
+    name: string;
+    transactionType: TransactionType;
+    quantity: number;
+    price: number;
+    transactionDate: DateAsString;
+    platform: Platform;
+    realizedProfit: number | null;
+    unrealizedProfit: number;
+    averageCost: number | null;
+    remainingQuantity: number;
+    commission: number;
+    currency: string;
+}
+
+export interface PortfolioSummaryDto {
+    date: DateAsString;
+    totalValue: number;
+    xirrAnnualReturn: number;
+    realizedProfit: number;
+    unrealizedProfit: number;
+    totalProfit: number;
+    earningsPerDay: number;
+    earningsPerMonth: number;
+    totalProfitChange24h: number | null;
+}
+
+export interface EtfHoldingBreakdownDto extends Serializable {
+    holdingTicker: string | null;
+    holdingName: string;
+    percentageOfTotal: number;
+    totalValueEur: number;
+    holdingSector: string | null;
+    inEtfs: string;
+    numEtfs: number;
+}
+
+export interface Serializable {
+}
+
+type DateAsString = string;
+
+export enum Platform {
+    AVIVA = "AVIVA",
+    BINANCE = "BINANCE",
+    COINBASE = "COINBASE",
+    IBKR = "IBKR",
+    LHV = "LHV",
+    LIGHTYEAR = "LIGHTYEAR",
+    SWEDBANK = "SWEDBANK",
+    TRADING212 = "TRADING212",
+    UNKNOWN = "UNKNOWN",
+}
+
+export enum ProviderName {
+    ALPHA_VANTAGE = "ALPHA_VANTAGE",
+    BINANCE = "BINANCE",
+    FT = "FT",
+    LIGHTYEAR = "LIGHTYEAR",
+    TRADING212 = "TRADING212",
+}
+
+export enum TransactionType {
+    BUY = "BUY",
+    SELL = "SELL",
+}
+
+export enum PriceChangePeriod {
+    P24H = "P24H",
+    P48H = "P48H",
+    P3D = "P3D",
+    P7D = "P7D",
+    P30D = "P30D",
+    P1Y = "P1Y",
+}
