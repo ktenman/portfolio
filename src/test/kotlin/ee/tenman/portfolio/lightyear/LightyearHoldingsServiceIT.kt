7ebcc346b447fe5982bea38e69c154189ab03374
Konstantin Tenman <ktenman@users.noreply.github.com>
2025-12-10 17:37:02 +0200

    Add support for parsing European currency patterns in holdings data

--- src/test/kotlin/ee/tenman/portfolio/lightyear/LightyearHoldingsServiceIT.kt
+++ src/test/kotlin/ee/tenman/portfolio/lightyear/LightyearHoldingsServiceIT.kt
@@ -1,5 +1,6 @@
 package ee.tenman.portfolio.lightyear
 
+import ch.tutteli.atrium.api.fluent.en_GB.notToEqualNull
 import ch.tutteli.atrium.api.fluent.en_GB.toBeGreaterThan
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
 import ch.tutteli.atrium.api.fluent.en_GB.toEqualNumerically
@@ -142,5 +143,206 @@ class LightyearHoldingsServiceIT {
 
     expect(holdings.size).toEqual(1)
     expect(holdings[0].logoUrl).toEqual("https://example.com/bg-logo.png")
+  }
+
+  @Test
+  fun `should parse Swiss Franc currency pattern with middle dot separator`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>Swiss Prime Site AG Fr. SPSN·Real Estate</div>
+          <div></div>
+          <div></div>
+          <div>1.5%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("Swiss Prime Site AG")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("SPSN")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Real Estate")
+    expect(holdings[0].weight).toEqualNumerically(BigDecimal("1.5"))
+  }
+
+  @Test
+  fun `should parse Danish Krone currency pattern`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>Danske Bank A/S DKr DANSKE·Finance</div>
+          <div></div>
+          <div></div>
+          <div>2.0%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("Danske Bank A/S")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("DANSKE")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Finance")
+  }
+
+  @Test
+  fun `should parse Swedish Krona currency pattern`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>Sandvik AB Skr SAND·Industrials</div>
+          <div></div>
+          <div></div>
+          <div>0.8%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("Sandvik AB")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("SAND")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Industrials")
+  }
+
+  @Test
+  fun `should parse Norwegian Krone currency pattern`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>Equinor ASA Nkr EQNR·Energy</div>
+          <div></div>
+          <div></div>
+          <div>1.2%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("Equinor ASA")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("EQNR")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Energy")
+  }
+
+  @Test
+  fun `should parse Hungarian Forint currency pattern`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>OTP Bank Nyrt. Ft OTP·Finance</div>
+          <div></div>
+          <div></div>
+          <div>0.5%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("OTP Bank Nyrt.")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("OTP")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Finance")
+  }
+
+  @Test
+  fun `should parse Polish Zloty currency pattern`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>PKO Bank Polski S.A zł PKO·Finance</div>
+          <div></div>
+          <div></div>
+          <div>0.3%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("PKO Bank Polski S.A")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("PKO")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Finance")
+  }
+
+  @Test
+  fun `should parse multi-word sector in European currency pattern`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>Temenos AG Fr. TEMN·Software & Cloud Services</div>
+          <div></div>
+          <div></div>
+          <div>0.4%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("Temenos AG")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("TEMN")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Software & Cloud Services")
+  }
+
+  @Test
+  fun `should parse company name with special characters in European currency pattern`() {
+    val sampleHtml =
+      """
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <div class="table-row">
+          <div>Chocoladefabriken Lindt & Sprüngli AG Fr. LISN·Consumer Essentials</div>
+          <div></div>
+          <div></div>
+          <div>0.2%</div>
+        </div>
+      </body>
+      </html>
+      """.trimIndent()
+
+    val holdings = lightyearHoldingsService.parseHoldings(sampleHtml, 1)
+
+    expect(holdings.size).toEqual(1)
+    expect(holdings[0].name).toEqual("Chocoladefabriken Lindt & Sprüngli AG")
+    expect(holdings[0].ticker).notToEqualNull().toEqual("LISN")
+    expect(holdings[0].sector).notToEqualNull().toEqual("Consumer Essentials")
   }
 }
