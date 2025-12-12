9a0d86743aede5c78b7e532e1c8a5c87e941bce7
Konstantin Tenman <ktenman@users.noreply.github.com>
2025-12-12 22:39:12 +0200

    Add BNBEUR instrument and initial buy transaction (#1061)
    * Add BNBEUR instrument and initial buy transaction
    
    Closes #1060
    
    * Update BNBEUR quantity to 0.1330711

---
+++ src/main/resources/db/migration/V202512121400__add_bnbeur_instrument.sql
@@ -0,0 +1,31 @@
+INSERT INTO instrument (
+    symbol,
+    name,
+    instrument_category,
+    base_currency,
+    provider_name,
+    current_price,
+    created_at,
+    updated_at,
+    version
+) VALUES (
+    'BNBEUR',
+    'BNB/EUR',
+    'Cryptocurrency',
+    'EUR',
+    'BINANCE',
+    747.93,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP,
+    0
+);
+
+INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date, platform)
+VALUES (
+    (SELECT id FROM instrument WHERE symbol = 'BNBEUR'),
+    'BUY',
+    0.1330711,
+    747.9257,
+    '2025-12-12',
+    'BINANCE'
+);
