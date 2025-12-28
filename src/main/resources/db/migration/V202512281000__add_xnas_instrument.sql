b15476c25a8439c7242b22a99ec46b4718d8a478
Konstantin Tenman <ktenman@users.noreply.github.com>
2025-12-28 18:26:38 +0200

    Add documentation header to migration file

--- src/main/resources/db/migration/V202512281000__add_xnas_instrument.sql
+++ src/main/resources/db/migration/V202512281000__add_xnas_instrument.sql
@@ -1,3 +1,8 @@
+-- Add Xtrackers Nasdaq 100 instrument
+-- Lightyear ID: 1f098996-8150-6170-8254-e77b526cb347
+-- Lightyear URL: https://lightyear.com/en/etf/XNAS:XETRA
+-- FT Symbol ID: 640687109
+
 INSERT INTO instrument (
     symbol,
     name,
