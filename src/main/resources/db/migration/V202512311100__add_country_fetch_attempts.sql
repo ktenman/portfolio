59fcf6f8df14693970f35a35d313d03249927884
Konstantin Tenman <konstantin.tenman@cyber.ee>
2025-12-31 13:07:51 +0200

    Add partial index for country classification query performance

--- src/main/resources/db/migration/V202512311100__add_country_fetch_attempts.sql
+++ src/main/resources/db/migration/V202512311100__add_country_fetch_attempts.sql
@@ -1,1 +1,3 @@
 ALTER TABLE etf_holding ADD COLUMN country_fetch_attempts INTEGER NOT NULL DEFAULT 0;
+
+CREATE INDEX idx_etf_holding_country_attempts ON etf_holding(country_fetch_attempts) WHERE country_code IS NULL;
