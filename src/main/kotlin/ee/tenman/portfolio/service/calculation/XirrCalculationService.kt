2e746e98a2d40cc3201b6a1ab5117f432554f39f
Konstantin Tenman <konstantin.tenman@cyber.ee>
2026-01-07 14:17:03 +0200

    Adjust XIRR calculation parameters for improved accuracy

--- src/main/kotlin/ee/tenman/portfolio/service/calculation/XirrCalculationService.kt
+++ src/main/kotlin/ee/tenman/portfolio/service/calculation/XirrCalculationService.kt
@@ -19,8 +19,8 @@ class XirrCalculationService(
   private val log = LoggerFactory.getLogger(javaClass)
 
   companion object {
-    private const val MIN_DAYS_FOR_XIRR = 365.25 / 4
-    private const val FULL_DAMPING_DAYS = 365.25 / 2
+    private const val MIN_DAYS_FOR_XIRR = 365.25 / 12
+    private const val FULL_DAMPING_DAYS = 365.25 / 6
   }
 
   fun buildCashFlows(
