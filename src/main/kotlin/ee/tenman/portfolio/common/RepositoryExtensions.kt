4d71285c6c0b9fa83510b8dd53c1e44a6d9ca98d
Konstantin Tenman <konstantin.tenman@cyber.ee>
2026-01-07 19:59:50 +0200

    Fix ktlint formatting

--- src/main/kotlin/ee/tenman/portfolio/common/RepositoryExtensions.kt
+++ src/main/kotlin/ee/tenman/portfolio/common/RepositoryExtensions.kt
@@ -3,16 +3,14 @@ package ee.tenman.portfolio.common
 import ee.tenman.portfolio.exception.EntityNotFoundException
 import java.util.Optional
 
-inline fun <reified T> Optional<T>.orThrow(id: Any): T =
-  orElseThrow { EntityNotFoundException(T::class.simpleName ?: "Entity", id) }
+inline fun <reified T> Optional<T>.orThrow(id: Any): T = orElseThrow { EntityNotFoundException(T::class.simpleName ?: "Entity", id) }
 
 inline fun <reified T> Optional<T>.orThrowByField(
   field: String,
   value: Any,
 ): T = orElseThrow { EntityNotFoundException(T::class.simpleName ?: "Entity", value, field) }
 
-inline fun <reified T : Any> T?.orThrow(id: Any): T =
-  this ?: throw EntityNotFoundException(T::class.simpleName ?: "Entity", id)
+inline fun <reified T : Any> T?.orThrow(id: Any): T = this ?: throw EntityNotFoundException(T::class.simpleName ?: "Entity", id)
 
 inline fun <reified T : Any> T?.orThrowByField(
   field: String,
