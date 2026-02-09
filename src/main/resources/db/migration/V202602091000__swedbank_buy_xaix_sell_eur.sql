d9fa97de04b7c7b2124ae968d811fa18602bdfee
Konstantin Tenman <konstantin.tenman@cyber.ee>
2026-02-09 11:28:52 +0200

    Fix EUR sell amount to 2876.22

--- src/main/resources/db/migration/V202602091000__swedbank_buy_xaix_sell_eur.sql
+++ src/main/resources/db/migration/V202602091000__swedbank_buy_xaix_sell_eur.sql
@@ -1,4 +1,4 @@
 INSERT INTO portfolio_transaction (instrument_id, transaction_type, quantity, price, transaction_date, platform, commission)
 VALUES
     ((SELECT id FROM instrument WHERE symbol = 'XAIX:GER:EUR'), 'BUY', 19, 151.38, '2026-02-09', 'SWEDBANK', 4.03),
-    ((SELECT id FROM instrument WHERE symbol = 'EUR'), 'SELL', 2880.25, 1.00, '2026-02-09', 'SWEDBANK', 0);
+    ((SELECT id FROM instrument WHERE symbol = 'EUR'), 'SELL', 2876.22, 1.00, '2026-02-09', 'SWEDBANK', 0);
